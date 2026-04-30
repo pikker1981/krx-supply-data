a799cf85bc94b531a477b9598da5fc9122fd3035
pikker1981 <dongjin12580@gmail.com>
2026-04-30 15:30:36 +0900

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,1 +1,36 @@
+const CACHE_NAME = "krx-supply-note-v1";
 
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./latest.json",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
