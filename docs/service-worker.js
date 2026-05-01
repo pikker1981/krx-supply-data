46d5d47239219eac3bcae66cece3b30e9d7d764f
pikker1981 <dongjin12580@gmail.com>
2026-05-01 18:02:21 +0900

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,36 +1,17 @@
-const CACHE_NAME = "krx-supply-note-v1";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./latest.json",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE_VERSION = "krx-supply-no-cache-v1";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    )
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
